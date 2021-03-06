565523ba113ae067b343f39611472a0f205bc9bc
Caitlin Tighe <caitighe@gmail.com>
2021-03-06 13:51:49 -0500

    added time blocks to local storage

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // THEN the current day is displayed at the top of the calendar WHEN I scroll down  //
 // Creating variables for the time and date //
 
-
-
-var currentDate;
-var currentDay;
-var currentTime;
 var currentHour;
-var currentSeconds;
+var time;
+var header = document.getElementById("todaysdate").append(newDate());
+
+$(document).ready(function() {
+    
+
+    //creating save button//
+
+    $(".saveBtn").on("click", function() {
+        
+        var time = $(this).parent().attr("id");
+        var value = $(this).siblings("description").val();
+
+        localStorage.setItem(time, value);
+
+    }
+})
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 
 
 
