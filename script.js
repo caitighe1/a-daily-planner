f628221fd28cc72f24a0994d5375229c0c4626b3
Caitlin Tighe <caitighe@gmail.com>
2021-03-06 22:28:37 -0500

    completed style.js , css and html, still not working properly

--- script.js
+++ script.js
@@ -1,105 +1,49 @@
-// THEN the current day is displayed at the top of the calendar WHEN I scroll down  //
-// Creating variables for the time and date //
+$(document).ready(function () {
 
-var currentHour;
-var time;
-var header = document.getElementById("todaysdate").append(newDate());
+    function currentDate() {
+        var momentDate = moment().format('LLLL');
+        $("#currentDay").text(momentDate);
+    };
+    currentDate();
 
-$(document).ready(function() {
-    
+    var date = new Date();
 
-    //creating save button//
+    var currentHour = date.getHours();
 
-    $(".saveBtn").on("click", function() {
-        
-        var time = $(this).parent().attr("id");
-        var value = $(this).siblings("description").val();
+    $(".userInput").each(function () {
 
-        localStorage.setItem(time, value);
+        if ($(this).attr("time") == currentHour) {
+            $(this).addClass("present");
+        };
 
-    }
-})
-
-function currentHour() {
-    var currentHour = moment().hours();
-
-    $(".time-block").each(function() {
-        var rowHour = parseInt($(this).attr("id").split("-")[1]);
-
-        if(rowkHour < currentHour) {
-            $(this).addClass("past")
-
-        } else if (rowHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
+        if ($(this).attr("time") > currentHour) {
             $(this).addClass("future");
-        }
-    }
-}
+        };
 
-currentTime();
+        if ($(this).attr("time") < currentHour) {
+            $(this).addClass("past");
+        };
 
-var interval = setInterval(currentTime, 15000);
+    });
+    var planLogs = JSON.parse(localStorage.getItem("planLogs")) || [];
+    // console.log(planLogs);
 
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    var areaText = $("textArea[name='text'");
+    // console.log(areaText);
 
+    $(areaText).each(function () {
+        $(this).val(planLogs[$(this).attr("time")]);
+    });
 
-$("currentDay").text(moment().format("dddd, MMMM"));
+    $("button").on("click", function () {
 
+        $(areaText).each(function () {
+            (planLogs)[$(this).attr("time")] = $(this).val();
 
+            localStorage.setItem("planLogs", JSON.stringify(planLogs));
 
-
-
-
-
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// THEN I am presented with timeblocks for standard business hours  //
-// WHEN I view the timeblocks for that day  //
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future  //
-// WHEN I click into a timeblock  //
-// THEN I can enter an event  //
-// WHEN I click the save button for that timeblock  //
-// THEN the text for that event is saved in local storage  //
-// WHEN I refresh the page  //
-// THEN the saved events persist  //
+    });
+});
